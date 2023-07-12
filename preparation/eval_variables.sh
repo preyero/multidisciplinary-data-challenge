8c38bdfbf083746fa9870e92227d1dadaebbdc4d
preyero <prl.sahelices@gmail.com>
2023-07-12 16:06:31 +0100

    computing resulting evaluation from team predictions

---
+++ preparation/eval_variables.sh
@@ -0,0 +1,4 @@
+DATASET_NAME=nobiaschallenge
+
+
+ANSWER_FILE_DIR=$(pwd)/datathon_results/answers/team_1
---
+++ preparation/gen_variables.sh
@@ -0,0 +1,10 @@
+AAA_FILE_DIR=$(pwd)/mhs
+TRAINING_SET=aaa_train.csv
+TEST_SET=aaa_test.csv
+DATASET_NAME=data-challenge
+
+echo $AAA_FILE_DIR
+
+# Generate :
+# test_names = ['quoting_a_to_n', 'flip_n_to_a', 'corr_a_to_a', 'corr_n_to_n']
+# other = ['f1_o', 'hashtag_check']
